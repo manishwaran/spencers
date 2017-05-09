0c7c7ead5b7530b2a587c9e9e1ccddb4631ae9d3
manishwaran <manishwaran.np@gmail.com>
2017-05-09 18:08:32 +0530

    Adding redirect.

--- app/views/start-page/index.jsx
+++ app/views/start-page/index.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
+import { browserHistory } from 'react-router';
 
 import './style.scss';
 import propTypes from './proptypes';
+import Toast from '../../components';
+import { UtilsStore } from '../../stores';
 import logo from '../../assests/images/spencers-logo.png';
 
 export default class StartPage extends Component {
 
   static propTypes = propTypes;
+
+  componentWillMount() {
+    if (this.props.location.pathname === '/') {
+      browserHistory.push('/expences');
+    }
+  }
 
   render() {
     const title = (
@@ -18,7 +27,7 @@ export default class StartPage extends Component {
         SPENCERS
       </div>
     );
-
+    console.log(UtilsStore);
     return (
       <div className="view-start-page">
         <AppBar title={title} showMenuIconButton={false} style={{ paddingLeft: 10 }} />
@@ -28,3 +37,5 @@ export default class StartPage extends Component {
   }
 
 }
+
+// {/* <Toast show={UtilsStore.SHOW_TOAST} message={UtilsStore.TOAST_MESSAGE} /> */}
